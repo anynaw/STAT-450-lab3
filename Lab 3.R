9c80a10a95070036bb66640ec4f99233a919cbf4
anynaw <anynac@icloud.com>
2025-01-24 15:50:59 -0800

    Last plot for the last lab

---
+++ Lab 3.R
@@ -0,0 +1,14 @@
+# Run this to load all necessary libraries
+library(tidyverse)
+# install.packages("palmerpenguins") # Run this if you can't load the palmerpenguins library 
+library(palmerpenguins)
+pgs <- penguins %>% drop_na
+head(pgs)
+
+pgs %>% 
+  ggplot() +
+  geom_point(aes(x = bill_length_mm, y = bill_depth_mm, color = species),
+             size = 2, alpha = 0.7) +
+  labs(x = "Bill Length (mm)",
+       y = "Bill Depth (mm)",
+       color = "Species")
